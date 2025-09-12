f95424d9690e6696d86d3138b296e51ba96a8fe2
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-09-12 10:50:50 +0545

    Map_Polyfill

--- Javascript/Map_Filter_Reduce/Map.js
+++ Javascript/Map_Filter_Reduce/Map.js
@@ -1,6 +1,19 @@
 const nums = [1,2,3,4,5];
 
-const multiply = nums.map((n) => n * n );
+// const multiply = nums.map((n) => n * n );
 
-console.log("Multiply : \n", multiply);
+// console.log("Multiply : \n", multiply);
 console.log(`Actual Array : \n ${nums}`);
+
+
+//Polyfill for map 
+
+Array.prototype.myOwnMap = function(cb) {
+	let temp = [];
+	for(let i = 0; i < this.length; i++){
+		temp.push(cb(this[i], i, this));
+	}
+	return temp;
+};
+
+console.log(nums.myOwnMap((n)=> n * n));
