ee964aa9c4820955d237803aaeb32745db279ca0
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-09-12 11:31:57 +0545

    Filter_Polyfill

--- Javascript/Map_Filter_Reduce/Filter.js
+++ Javascript/Map_Filter_Reduce/Filter.js
@@ -9,3 +9,25 @@ console.log(`Actual Arrary : \n${numArr}`);
 console.log(`Odd Number Array : ${oddNumArr}`);
 
 console.log(`Even Number Array :  ${evenNumArr}`);
+
+
+// Polyfill For Filter
+
+Array.prototype.myfilter = function(cb){
+	let temp = [];
+	
+	for(let i = 0; i < this.length; i++){
+		if(cb(this[i],i,this))
+			temp.push(this[i])
+
+	}
+	return temp;
+};
+
+const newNum = [1,2,3,4,5];
+
+const Arr = newNum.myfilter((n) => n > 2);
+
+console.log(Arr);
+
+
