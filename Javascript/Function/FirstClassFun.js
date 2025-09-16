2f8bdd32772f7a6a1d71a2934035b90faaf3aadc
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-09-16 12:02:34 +0545

    first_class_Funtion

--- Javascript/Function/FirstClassFun.js
+++ Javascript/Function/FirstClassFun.js
@@ -6,3 +6,13 @@ function square(num){
 
 console.log(square(5));
 console.log(square(10));
+
+
+//First Class Function :function that is treated like any other variable
+
+
+function display(fn){
+	console.log(`Square is ${fn(6)}`);
+}
+
+display(square);
