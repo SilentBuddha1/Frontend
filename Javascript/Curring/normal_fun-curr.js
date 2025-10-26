6369a33c3381a0f9e75d28fe4b1f16321c516cb7
Ramchandra Karki <106328401+SilentBuddha1@users.noreply.github.com>
2025-10-26 04:34:55 +0000

    normal function to curring

---
+++ Javascript/Curring/normal_fun-curr.js
@@ -0,0 +1,18 @@
+function c(fn) {
+    return function curried(...args) {
+        if (args.length >= fn.length) {
+            return fn(...args);
+        }
+        return function(...next) {
+            return curried(...args, ...next);
+        }
+    }
+}
+
+const sum = (a, b, c) => a + b + c;
+
+const curriedSum = c(sum);
+
+console.log(curriedSum(1)(2)(3)); 
+console.log(curriedSum(1, 2)(3)); 
+console.log(curriedSum(1, 2, 3));
