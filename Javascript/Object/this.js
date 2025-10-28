e8b11c407e9f71a1629a3af54d7ed936edc9b7e8
Ramchandra Karki <106328401+SilentBuddha1@users.noreply.github.com>
2025-10-28 06:28:48 +0000

    this behave

---
+++ Javascript/Object/this.js
@@ -0,0 +1,10 @@
+// Output
+const shape = {
+    radius: 10,
+    diameter() { return this.radius * 2; },
+    perimeter: () => 2 * Math.PI * this.radius,
+};
+
+console.log(shape.diameter()); // Normal function this refers to the shape object 
+console.log(shape.perimeter()); // Arrow function this refers to the global object
+
