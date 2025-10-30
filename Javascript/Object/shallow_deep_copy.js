18da5193646407e540f45b8206925a401c7389df
Ramchandra Karki <106328401+SilentBuddha1@users.noreply.github.com>
2025-10-30 09:26:23 +0000

    another method

--- Javascript/Object/shallow_deep_copy.js
+++ Javascript/Object/shallow_deep_copy.js
@@ -9,3 +9,18 @@ objClone.name = "Shyam";
 
 console.log("Original Object:", user);
 console.log("Cloned Object:", objClone);
+
+
+let objClone2 = {...user};
+
+objClone2.age = 30;
+
+console.log("Original Object after spread operator clone:", user);
+console.log("Cloned Object after spread operator clone:", objClone2);
+
+let objClone3 = JSON.parse(JSON.stringify(user));
+
+objClone3.name = "Krishna";
+
+console.log("Original Object after JSON clone:", user);
+console.log("Cloned Object after JSON clone:", objClone3);
