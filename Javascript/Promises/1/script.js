f044e03736d3451060cf6e2531a98d9150e27ade
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-11-06 09:25:42 +0545

    learning

---
+++ Javascript/Promises/1/script.js
@@ -0,0 +1,15 @@
+//Synchronous JavaScript Example
+console.log("Hello, World!");
+
+console.log("This is a sample JavaScript file.");
+
+console.log("It demonstrates basic logging functionality.");
+
+
+//Asynchronous JavaScript Example 
+
+setTimeout(() => {
+    console.log("This message is displayed after a delay.");
+}, 1000);
+
+console.log("This message is displayed immediately.");
