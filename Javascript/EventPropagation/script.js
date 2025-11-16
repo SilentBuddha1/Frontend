0e5f96b0381a63f13bdc40d1cebd580dd452d685
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-11-16 16:34:28 +0545

    Event Propagation

---
+++ Javascript/EventPropagation/script.js
@@ -0,0 +1,15 @@
+const div = document.querySelector("div");
+const form = document.querySelector("form");
+const button = document.querySelector("button");
+
+div.addEventListener("click", function(){
+    alert("Div");
+})
+
+form.addEventListener("click", function(){
+    alert("form");
+})
+
+button.addEventListener("click", function() {
+    alert("Button");
+})
