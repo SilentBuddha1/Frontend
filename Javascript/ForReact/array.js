d32cb844343b1800880d489ea20fd48905eaa386
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-11-18 14:05:32 +0545

    copying

--- Javascript/ForReact/array.js
+++ Javascript/ForReact/array.js
@@ -12,9 +12,22 @@
 
 let state = [1,2,3,4];
 
-let copy = [...state]; //Destructuring
+let copy = [...state]; //Destructuring by spread
 
 copy.pop();
 
 console.log(state);
 console.log(copy);
+
+var obj = {
+    name: "Ram",
+    age: 24,
+}
+
+let copyObj = {...obj};
+
+copyObj.name = "Hari";
+
+console.log(obj);
+
+console.log(copyObj);
